67c51eba0f0f14e7f523f50815cade1a246162e4
John <jbattagl@umich.edu>
2014-11-05 12:57:13 -0500

    adding index.php and styles.css

--- public/index.php
+++ public/index.php
@@ -1,9 +1,49 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
+
+<!-- 	John Battaglia
+		Eric Gardner
+		Ahmed Altai
+		CIS 435 - Fall 2014
+		Term Project
+		File: index.html
+							-->
 <html>
-	<head>
-		<title> Radio Website </title>
-	</head>
-	<body>
-		<p> Hello! </p>
-	</body>
+<head>
+    <title>UMD Radio Station</title>
+    <link rel="stylesheet" id="stylesheet" type="text/css" href="styles.css" />
+</head>
+ 
+<body>
+<div id="body">
+	<div id="wrapper">
+		<div id="header">
+			<div id="siteName">
+			Radio Site Name
+			</div>
+			<div id="login">
+				<a href="">Login</a>
+			</div>
+			<div id="live">
+			</div>
+		</div>
+		<div id="nav">
+			<ul>
+			  <li><a href="">Home</a></li>
+			  <li><a href="">Playlist</a></li>
+			  <li><a href="">Recently Played Songs</a></li>
+			  <li><a href="">Top 10</a></li>
+			  <li><a href="">Chatroom</a></li>
+			</ul>
+		</div>
+		<div id="section">
+			<h1>The body</h1>
+		</div>
+		<div id="footer">
+			<span>Internet Radio Station | University Of Michigan - Dearborn</span>
+			<p>Copyright &copy; 2014. All Rights Reserved</p>
+
+		</div>
+	</div>
+</div>
+</body>
 </html>
