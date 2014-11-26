6a1d51d125e57842fd6314680052a31f1d7c94d7
Eric Gardner <eagard@umich.edu>
2014-11-25 20:55:10 -0500

    changed lower case to uppercase because

--- public/includes/nav.php
+++ public/includes/nav.php
@@ -2,7 +2,7 @@
 	<ul>
 		<li><a href="index.php">Home</a></li>
 		<li><a href="search.php">Search</a></li>
-		<li><a href="recentSongs.php">Recently Played Songs</a></li>
+		<li><a href="recentsongs.php">Recently Played Songs</a></li>
 		<li><a href="top10.php">Top 10</a></li>
 		<li><a href="chat.php">Chatroom</a></li>
 		<li><a href="browse.php">Browse Songs</a>
