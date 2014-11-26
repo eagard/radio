6e859a9d6379d73f8432e5e1deed67d1cef725b4
John Battaglia <jbattagl@umich.edu>
2014-11-25 19:56:00 -0500

    Changed columns in recentsongs page.

--- public/scripts/database/top10.php
+++ public/scripts/database/top10.php
@@ -2,33 +2,49 @@
 
 <table>
     <tr>
-		<th>Stars</th>
 		<th>Title</th>
 		<th>Composer</th>
 		<th>Performer</th>
-		<th>Rank</th>		
+		<th style='width:164px;'>User Rating</th>
+		<th style='text-align:center;margin-left:auto;margin-right:auto;'>Source</th>	
     </tr>
 
 <?php
 
 // build SELECT query
-$query = "SELECT stars, title, composer, performer, rank FROM `RATING`,`SONG` WHERE SONG.id = RATING.song ORDER BY stars DESC LIMIT 10"; 
+$query = "SELECT * FROM `RATING`,`SONG` WHERE SONG.id = RATING.song ORDER BY stars DESC LIMIT 10"; 
 
 if($query_run = mysql_query($query)) {
 	while($query_row = mysql_fetch_assoc($query_run)) {
 		$title = $query_row['title'];
 		$composer = $query_row['composer'];
 		$performer = $query_row['performer'];
-		$rank = $query_row['rank'];
-		$stars = $query_row['stars'];
+		$stars = 3;
+		$source = $query_row['source'];
 	
 ?>
 	<tr>
-		<td><?php echo $stars;?></td>
 		<td><?php echo $title;?></td>
 		<td><?php echo $composer;?></td>
 		<td><?php echo $performer;?></td>
-		<td><?php echo $rank;?></td>
+		<td><?php
+			for ($i = 0; $i < 5; $i++)
+			{
+				if ($i < $stars)
+				{
+					echo "<img src='images/star_full.png' alt='x'/>";
+				}
+				else
+				{
+					echo "<img src='images/star_empty.png' alt='o'/>";
+				}
+			}
+		?></td>
+		<td style='text-align:center;'>
+			<a href='<?php echo "http://".$source; ?>'>
+				<img src='images/arrow.png' alt='source'/>
+			</a>
+		</td>
 	</tr>
 
 <?php
