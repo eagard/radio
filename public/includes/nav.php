fd099eee3915de0554edd1d22836335848e0d93e
eagard <eagard@users.noreply.github.com>
2014-12-03 14:16:54 -0500

    removed chat nav link

--- public/includes/nav.php
+++ public/includes/nav.php
@@ -4,7 +4,6 @@
 		<li><a href="browse.php">Browse</a>
 		<li><a href="search.php">Search</a></li>
 		<li><a href="recentsongs.php">Recent</a></li>
-		<li><a href="top10.php">Top 10</a></li>
-		<li><a href="chat.php">Chatroom</a></li>
+		<li><a href="top10.php">Top Ten</a></li>
 	</ul>
 </div>
